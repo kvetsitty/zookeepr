517f2f790e56666833039609e42e53a69dbffb55
n1zhon1 <kristineetsitty@gmail.com>
2021-10-08 18:48:47 -0700

    updated server.js

--- server.js
+++ server.js
@@ -17,13 +17,6 @@ function filterByQuery(query, animalsArray) {
         }
         //loop through each trait in the personalityTraits array:
         personalityTraitsArray.forEach(trait => {
-            //check the trait against each animal in the filteredResults array.
-            //remember, it is initially a copy of the animalsArray,
-            //but here we're updating it for each trait in the .forEach() loop.
-            //for each trait being targeted by the filter, the filteredResults
-            //array will then contain only the entries that contain the trait,
-            //so at the end we'll have an array of animals that have every one
-            //of the traits when the .forEach() loop is finished.
             filteredResults = filteredResults.filter(
                 animal => animal.personalityTraits.indexOf(trait) !== -1
             );
@@ -62,6 +55,8 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404);
     }
 });
+
+app.post('/api/animals', (req, res) => {});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
